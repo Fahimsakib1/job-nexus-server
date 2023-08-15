2189d0fed652fc7aa01620e8e4bf304f83559b4b
Fahim <60335100+Fahimsakib1@users.noreply.github.com>
2023-08-15 07:44:00 +0600

    API Added

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ async function run() {
         const usersCollection = client.db('Moon_Tech_Redux_Thunk').collection('users');
         const jobsCollection = client.db('Moon_Tech_Redux_Thunk').collection('jobs');
 
-    
+        //add user after sign up
         app.post('/addUser', async (req, res) => {
             const user = req.body;
             console.log(user);
@@ -48,6 +48,54 @@ async function run() {
                 return res.send({ acknowledged: false, message });
             }
             const result = await usersCollection.insertOne(user);
+            res.send(result);
+        })
+
+        //Get All jobs
+        app.get('/allJobs', async (req, res) => {
+            const query = {};
+            const result = await jobsCollection.find(query).toArray()
+            res.send(result);
+        })
+
+        //post job
+        app.post('/addJob', async (req, res) => {
+            const jobDetails = req.body;
+            const result = await jobsCollection.insertOne(jobDetails)
+            res.send(result);
+        })
+
+        //Get a specific job
+        app.get('/allJobs/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.findOne(query)
+            res.send(result);
+        })
+
+        //Get specific jobs by user email
+        app.get('/jobsByEmail/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { userEmail: email};
+            const result = await jobsCollection.find(query).toArray()
+            res.send(result);
+        })
+
+        //delete a specific job
+        app.delete('/job/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.deleteOne(query)
+            res.send(result);
+        })
+
+
+
+        //Get specific jobs by user email
+        app.get('/myJobs/:email', async (req, res) => {
+            const email = req.params.email
+            const query = { userEmail: email};
+            const result = await jobsCollection.find(query).toArray()
             res.send(result);
         })
 
